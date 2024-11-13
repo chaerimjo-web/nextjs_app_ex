8676f6c07c04e941f9bc413b4b974d168f870b3e
chaerimjo-web <chaerim010710@gmail.com>
2024-11-13 15:35:29 +0900

    글 입력 후 이동

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,36 +1,35 @@
-"use client"; 
+"use client";
+import { useRouter } from 'next/navigation'
 
 export default function Create() {
+  const router = useRouter();
   const onSubmit = (e)=>{
     e.preventDefault();
     const title = e.target.title.value;
     const body = e.target.body.value;
     const options = {
-      method:'POST', //3번. 
+      method:'POST',
       headers:{
-        'Content-Type':'application/json' //2번. 바디로 넘어가는것이 제이슨형식으로
+        'Content-Type':'application/json'
       },
-      body:JSON.stringify({title, body}) //1번. 사용자가 입력한 제목과 내용이 title, body를 문자로 바꿔줌
-
-      /*
-      GET방식 -> http://localhost:3000/create?title=제목&바디=내용
-      POST방식 => title= 제목&바디=내용 /body에 다 숨겨서 나옴
-      */
+      body:JSON.stringify({title, body}) //object->json
     }
     fetch('http://localhost:9999/topics', options)
-    .then(res=>res.json()) //결과를 객체형식으로 object 변환해서 받는다.
-    .then(result=>{
-      console.log(result);
-    })
+      .then(res=>res.json()) //결과를 객체로 변환
+      .then(result=>{
+        console.log(result);
+        router.push(`/read/${result.id}`);
+        router.refresh();
+      })
   }
   return (
     <div>
       <form onSubmit={onSubmit}>
         <div>
-          <input type="text" name="title" placeholder="title" />
+          <input type="text" name="title" placeholder="title"/>
         </div>
         <div>
-          <textarea name="body" placeholder="content" ></textarea>
+          <textarea name="body" placeholder="content"></textarea>
         </div>
         <button type="submit">전송</button>
       </form>
